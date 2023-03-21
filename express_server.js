25d139ad8986c8fdea4c3e61eafb15ac8feee33e
Omar Hamid <omarhamid664@gmail.com>
2023-03-21 08:15:17 -0600

    added post route for urls/ endpoint

--- express_server.js
+++ express_server.js
@@ -9,6 +9,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -24,6 +26,15 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+})
+
+app.get("/urls/new", (req, res) => {
+  res.render('urls_new');
 });
 
 app.get("/urls/:id", (req, res) => {
