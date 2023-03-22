8df70a1783c3451f807268f1986abcba79154825
Omar Hamid <omarhamid664@gmail.com>
2023-03-21 21:09:17 -0600

    added cookie with username to every render method

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -10,6 +11,7 @@ const urlDatabase = {
 };
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Generates 6 random alpha-numeric characters
 function generateRandomString() {
@@ -48,7 +50,11 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies['username']
+  };
+
   res.render("urls_index", templateVars);
 });
 
@@ -60,7 +66,8 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render('urls_new');
+  const templateVars = { username: req.cookies['username'] }
+  res.render('urls_new', templateVars);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
@@ -70,7 +77,11 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies['username'],
+  };
   res.render("urls_show", templateVars);
 });
 
