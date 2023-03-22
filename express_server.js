e050702d91a5bb3e0e67cf65f6c3f3bbfb77a73b
Omar Hamid <omarhamid664@gmail.com>
2023-03-22 14:35:15 -0600

    added get route for '/register'

--- express_server.js
+++ express_server.js
@@ -37,6 +37,11 @@ app.post('/login', (req, res) => {
   res.redirect('/urls');
 });
 
+app.get('/register', (req, res) => {
+  const templateVars = { username: req.cookies['username'] }
+  res.render('register', templateVars);
+})
+
 app.post('/logout', (req, res) => {
   res.clearCookie('username');
   res.redirect('/urls');
@@ -48,10 +53,6 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
