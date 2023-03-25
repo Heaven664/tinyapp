29629e624b6a3e3c97f13a6b6afd2a7ca96814b4
Omar Hamid <omarhamid664@gmail.com>
2023-03-25 09:39:02 -0600

    move databases to separate file

---
+++ database.js
@@ -0,0 +1,26 @@
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+    totalVisits: 0,
+    uniqueVisitors: [],
+    logs: []
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+    totalVisits: 0,
+    uniqueVisitors: [],
+    logs: []
+  },
+};
+
+const users = {
+  aJ48lW: {
+    id: "aJ48lW",
+    email: "a@a.com",
+    password: "$2a$10$aD/LJN4yGT6KVWaN9Vn8UuwY.i8Rwzp21lXHBKuw4gcJ4zgHE3oPK",
+  },
+};
+
+module.exports = { urlDatabase, users }
--- express_server.js
+++ express_server.js
@@ -3,36 +3,13 @@ const bcrypt = require("bcryptjs");
 const cookieSession = require('cookie-session');
 const methodOverride = require('method-override');
 const { getUserByEmail, generateRandomString, addUser, urlsForUser } = require('./helpers');
+const { urlDatabase, users } = require('./database');
 
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
-  b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW",
-    totalVisits: 0,
-    uniqueVisitors: [],
-    logs: []
-  },
-  i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW",
-    totalVisits: 0,
-    uniqueVisitors: [],
-    logs: []
-  },
-};
-
-const users = {
-  aJ48lW: {
-    id: "aJ48lW",
-    email: "a@a.com",
-    password: "$2a$10$aD/LJN4yGT6KVWaN9Vn8UuwY.i8Rwzp21lXHBKuw4gcJ4zgHE3oPK",
-  },
-};
 
 // middleware 
 app.use(express.urlencoded({ extended: true }));
